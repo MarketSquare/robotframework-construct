6fa16a14c32092c209926831e4445696c338f86e
Franz Haas <franz.dominik.haas@gmail.com>
2024-11-10 21:36:26 +0100

    - less rounds

--- tests/__init__.py
+++ tests/__init__.py
@@ -30,7 +30,7 @@ def test_impossible_params():
     assert "protocol should be either `TCP or `UDP´, but was `raw´" == str(excinfo.value)
 
 def test_tcp_internals():
-    for _ in range(5):
+    for _ in range(2):
         r = robotframework_construct.reflector.reflector()
         ps = r.reflector(robotframework_construct.Protocol.TCP)
         for p in ps:
